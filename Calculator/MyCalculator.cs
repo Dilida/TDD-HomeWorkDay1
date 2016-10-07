02e025a0cdaaa7ae99e05107c82cdd229224cd70
Dilida <Dilida@DD>
2016-10-07 10:41:04 +0800

    加入專案檔案。

---
+++ Calculator/MyCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class MyCalculator
+    {
+        public ArrayList HomeWorkSum<T> (string columValue, int numberValue, IEnumerable<T> actualSum) //泛型才可以計算各種欄位
+        {
+            ArrayList columSum = new ArrayList();
+
+            int count = 0;
+            int sum = 0;  //宣告總和為零
+            foreach (var item in actualSum)
+            {
+                count++;
+                sum += Convert.ToInt32(item.GetType().GetProperty(columValue).GetValue(item).ToString()); //GetType型別 .toint32轉整數
+
+                if (count % numberValue == 0)
+                {
+                    columSum.Add(sum); //加總
+                    sum = 0; //重算
+                }
+
+
+            }
+
+            if (count % numberValue != 0 && count == actualSum.Count())
+            {
+                columSum.Add(sum); //補上最後一個加總
+            }
+
+            return columSum;
+           
+        }
+    }
+
+}
